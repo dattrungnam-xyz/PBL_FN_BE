339dc51bc1e0259ad9bc77d9d94abbc695e17e09
dattrungnam <datvtrg0510@gmail.com>
2025-06-15 22:14:19 +0700

    add service for location

--- src/location/location.controller.ts
+++ src/location/location.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get, Param } from '@nestjs/common';
+import { LocationService } from './location.service';
 @Controller('location')
-export class LocationController {}
+export class LocationController {
+    constructor(private readonly locationService: LocationService) {}
+    @Get("provinces")
+    async getProvinces() {
+        return this.locationService.getProvinces();
+    }
+
+    @Get("/districts/:provinceId")
+    async getDistricts(@Param("provinceId") provinceId: string) {
+        return this.locationService.getDistricts(provinceId);
+    }
+
+    @Get("/wards/:districtId")
+    async getWards(@Param("districtId") districtId: string) {
+        return this.locationService.getWards(districtId);
+    }
+}
--- src/location/location.service.ts
+++ src/location/location.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
-
+import { DataSource } from 'typeorm';
 @Injectable()
-export class LocationService {}
+export class LocationService {
+    constructor(private readonly datasource: DataSource) {
+    }
+
+    async getProvinces() {
+        const provinces = await this.datasource.query(`SELECT * FROM provinces`);
+        return { data: provinces };
+    }
+    
+    async getDistricts(provinceId: string) {
+        const districts = await this.datasource.query(`SELECT * FROM districts WHERE ProvinceId = ${provinceId}`);
+        return { data: districts };
+    }
+
+    async getWards(districtId: string) {
+        const wards = await this.datasource.query(`SELECT * FROM wards WHERE DistrictId = ${districtId}`);
+        return { data: wards };
+    }
+}
