f1dd50686b895ca7ec315554f133a678dac98335
dattrungnam <datvtrg0510@gmail.com>
2025-06-15 22:45:28 +0700

    fix: mapping location service

--- src/location/location.service.ts
+++ src/location/location.service.ts
@@ -7,16 +7,31 @@ export class LocationService {
 
     async getProvinces() {
         const provinces = await this.datasource.query(`SELECT * FROM provinces`);
-        return { data: provinces };
+        return { data: (provinces || []).map((province) => ({
+            id: province.Id,
+            slug: province.Slug,
+            name: province.Name,
+            type: province.Type,
+        })) };
     }
     
     async getDistricts(provinceId: string) {
         const districts = await this.datasource.query(`SELECT * FROM districts WHERE ProvinceId = ${provinceId}`);
-        return { data: districts };
+        return { data: (districts || []).map((district) => ({
+            id: district.Id,
+            provinceId: district.ProvinceId,
+            name: district.Name,
+            type: district.Type,
+        })) };
     }
 
     async getWards(districtId: string) {
         const wards = await this.datasource.query(`SELECT * FROM wards WHERE DistrictId = ${districtId}`);
-        return { data: wards };
+        return { data: (wards || []).map((ward) => ({
+            id: ward.Id,
+            districtId: ward.DistrictId,
+            name: ward.Name,
+            type: ward.Type,
+        })) };
     }
 }
